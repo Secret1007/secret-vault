d95769d63732be91109ede68c64ffcda09e452c5
mecrev-secret <18364307619@163.com>
2024-10-19 10:56:29 +0800

    feat: add Zustand store for state management

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import CreateWalletBox from "@/components/wallet/CreateWalletBox";
 import { useEffect, useState } from "react";
-import Login from "@/app/login/page";
+import LoginBox from "@/components/wallet/LoginBox";
+import CreateWalletBox from "@/components/wallet/CreateWalletBox";
+
 export default function Home() {
   const [hasWallet, setHasWallet] = useState(true);
+  // const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     // 只在客户端执行：检查 localStorage 中是否有钱包地址
@@ -17,15 +19,40 @@ export default function Home() {
       }
     }
   }, []);
+  // const toggleModal = () => {
+  //   setIsModalOpen(!isModalOpen);
+  // };
+
+  // const closeModal = () => {
+  //   setIsModalOpen(false);
+  // };
   return (
     <div className="flex justify-center items-center h-screen">
       {hasWallet ? (
-        <Login /> // 登录界面
+        <LoginBox /> // 登录界面
       ) : (
         <div className="w-1/4 mx-auto bg-white border border-purple-500 rounded-lg shadow-lg overflow-hidden">
           <CreateWalletBox />
         </div>
       )}
     </div>
+    // <div>
+    //   {/* Navbar 部分 */}
+    //   <Navbar toggleModal={toggleModal} />
+
+    //   {/* Content 部分 */}
+    //   <main className="p-8">
+    //     <h2 className="text-3xl font-bold mb-6">Transaction Content</h2>
+    //     <p>这里是交易页面的内容...</p>
+    //     {/* 你可以在这里添加交易相关的内容 */}
+    //   </main>
+
+    //   {/* Modal 弹出框 */}
+    //   <WalletModal
+    //     isOpen={isModalOpen}
+    //     closeModal={closeModal}
+    //     hasWallet={hasWallet}
+    //   />
+    // </div>
   );
 }
